6210aa18208f1f372bfd359e4fc16f5e47f29885
Singh2202 <71194920+Singh2202@users.noreply.github.com>
2021-01-15 12:09:09 -0500

    Update vector magnitude.py

--- vector magnitude.py
+++ vector magnitude.py
@@ -2,8 +2,8 @@ import math
 def magnitude(vector):
   """Returns magnitude of an n-dimensional vector.
   Example usage: magnitude([5,6,7])."""
-  sum = 0
+  total = 0
   for component in vector:
-    sum += component ** 2
-  magnitude = math.sqrt(sum)
+    total += component ** 2
+  magnitude = math.sqrt(total)
   return magnitude
