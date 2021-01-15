594ce908f8418b421bb838b5e9a8e070da070f3e
Singh2202 <71194920+Singh2202@users.noreply.github.com>
2021-01-15 11:57:25 -0500

    Update 3x3 determinant.py

--- 3x3 determinant.py
+++ 3x3 determinant.py
@@ -1,9 +1,13 @@
-three_by_three = [[], [], []]
-c1 = three_by_three[0][0]
-c2 = three_by_three[0][1]
-c3 = three_by_three[0][2]
-determinant_1 = three_by_three[2][2]*three_by_three[1][1] - three_by_three[2][1]*three_by_three[1][2]
-determinant_2 = three_by_three[2][2]*three_by_three[1][0] - three_by_three[2][0]*three_by_three[1][2]
-determinant_3 =  three_by_three[2][1]*three_by_three[1][0] - three_by_three[2][0]*three_by_three[1][1]
-determinant = c1*determinant_1 - c2*determinant_2 + c3*determinant_3
-print(determinant)
+def determinant(matrix):
+  """Returns the determinant of a 2X2 or 3X3 matrix. Example usage: determinant([[6,5,2], [4,3,2], [4,3,2]]) or determinant([[5,6], [3,3]])."""
+  if len(matrix) == 2:
+    determinant = matrix[1][1]*matrix[0][0] - matrix[1][0]*matrix[0][1]
+  elif len(matrix) == 3:
+    c1 = matrix[0][0]
+    c2 = matrix[0][1]
+    c3 = matrix[0][2]
+    determinant_1 = matrix[2][2]*matrix[1][1] - matrix[2][1]*matrix[1][2]
+    determinant_2 = matrix[2][2]*matrix[1][0] - matrix[2][0]*matrix[1][2]
+    determinant_3 =  matrix[2][1]*matrix[1][0] - matrix[2][0]*matrix[1][1]
+    determinant = c1*determinant_1 - c2*determinant_2 + c3*determinant_3
+  return determinant
